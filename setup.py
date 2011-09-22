6ecb0f9c707d674386a0672b7991115d9c68b355
Anthony Tresontani <tresontani@gmail.com>
2011-09-22 09:29:04 +0100

    add a basic setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='csvImporter',
+      version='0.1',
+      description='Convert csv files into python object or django model',
+      author='Anthony Tresontani',
+      author_email='dev.tresontani@gmail.com',
+      packages=['csvImporter'],
+     )
