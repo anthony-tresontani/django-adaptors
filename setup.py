2b524f6dd0ce257b85cf56af45d957b6eca32994
Anthony TRESONTANI <tresontani@gmail.com>
2011-09-24 20:47:57 +0100

    Modification of setup.py for to publish on Pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
-
+import os
 from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='csvImporter',
       version='0.1',
       description='Convert csv files into python object or django model',
       author='Anthony Tresontani',
       author_email='dev.tresontani@gmail.com',
+      long_description =read('README.txt'),
+      license = "BSD",
+      keywords = "CSV Django loader",
       packages=['csvImporter'],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
      )
