43d1758b983b0b12850648601524f93c304e36d0
Anthony TRESONTANI <tresontani@gmail.com>
2011-10-01 10:30:55 +0100

    Update in docs for Layout

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='csvImporter',
-      version='0.1',
+      version='0.1.1',
       description='Convert csv files into python object or django model',
       author='Anthony Tresontani',
       author_email='dev.tresontani@gmail.com',
