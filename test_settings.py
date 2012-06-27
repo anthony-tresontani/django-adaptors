612dbf005cb9b09dbb311d4f22f14bd5b20b55e0
David Winterbottom <david.winterbottom@gmail.com>
2012-06-27 13:22:22 +0100

    Updated docs with installation instructions for contributors.
    Plus minor change to setup.py and paths to modules

--- test_settings.py
+++ test_settings.py
@@ -118,8 +118,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'adaptors.adaptor',
-    'adaptors.myTestModel',
+    'adaptor',
+    'myTestModel',
     'django_nose',
 )
 
