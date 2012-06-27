6b8b8ab2a871f8b782a442100ab0a7d2a4611fc9
Anthony Tresontani <tresontani@gmail.com>
2012-06-27 10:05:57 +0200

    Release 0.2.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='django-adaptors',
-      version='0.2.0.8',
+      version='0.2.0.9',
       description='Convert CSV/XML files into python object or django model',
       author='Anthony Tresontani',
       author_email='dev.tresontani@gmail.com',
